fbcd4b1abf78cf309f31a239eeae8a6fea5ecdb1
cinjon <cinjon.resnick@gmail.com>
2014-12-24 16:53:36 -0800

    random shit like css and adding libraries and config.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
-from flask import Flask, request, Response
-from flask import render_template, send_from_directory, url_for
+from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.mobility import Mobility
-from logging import StreamHandler, DEBUG
 import utility
 import config
 
@@ -11,12 +9,9 @@ baseurl = config.baseurl
 
 flask_app = Flask(__name__, template_folder='public/template')
 flask_app.config.from_object('config')
+flask_app.debug = True
 db = SQLAlchemy(flask_app)
 Mobility(flask_app)
-
-file_handler = StreamHandler()
-flask_app.logger.setLevel(DEBUG)  # set the desired logging level here
-flask_app.logger.addHandler(file_handler)
 
 import models
 import controllers
